b91427e4ae16ec49873f4b4e5405d745ab214b66
hnsa9 <hanisa.nine@gmail.com>
2024-03-14 09:52:54 +0800

    remove poppler and tesseract hardcoded paths

--- app/main.py
+++ app/main.py
@@ -99,5 +99,5 @@ async def upload_base64(request_data: ExtractionRequest):
 # Entry point to run the application when executed directly
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
